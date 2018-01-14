3834f7797af5205d3fe94cdb1811385c50380dcc
Alex Suciu <alexsuciu06@gmail.com>
2018-01-14 23:15:50 +0200

    Added puzzle and resources

---
+++ js/config.js
@@ -0,0 +1,74 @@
+var imgs = [
+	{
+		src: "./static/parts/ax_jos.png", 
+		name: "ax_jos", 
+		x: 787, 
+		y: 206 
+	}, 
+	{
+		src: "./static/parts/cadru.png", 
+		name: "cadru", 
+		x: 539, 
+		y: 3
+	}, 
+	{
+		src: "./static/parts/front_tyre.png", 
+		name: "front_tyre", 
+		x: 983, 
+		y: 47
+	}, 
+	{
+		src: "./static/parts/ghidon.png", 
+		name: "ghidon", 
+		x: 6, 
+		y: 6
+	}, 
+	{
+		src: "./static/parts/pedala.png", 
+		name: "pedala", 
+		x: 41, 
+		y: 289
+	}, 
+	{
+		src: "./static/parts/roata_spate.png", 
+		name: "roata_spate", 
+		x: 194, 
+		y: 16
+	},
+	{
+		src: "./static/parts/sezut.png", 
+		name: "sezut", 
+		x: 842, 
+		y: 28
+	} 
+];
+
+//Puzzle solutions for each part
+var solutions = {
+	ax_jos : {
+		x: 498, y: 465
+	},
+	pedala: {
+		x: 574, y: 609
+	},
+	cadru: {
+		x: 499, y: 425
+	},
+	roata_spate: {
+		x: 675, y: 499
+	},
+	sezut: {
+		x: 682, y: 405
+	},
+	front_tyre: {
+		x: 247, y: 487
+	},
+	ghidon: {
+		x: 415, y: 330
+	}
+}
+				
+			
+var sounds = {
+	ghidon: "./static/mp3/ghidon.m4a"
+};
---
+++ js/puzzle.js
@@ -0,0 +1,154 @@
+/*
+	This js uses some variables found in ./config.js.
+	Please include that before including this in your html.
+	
+	Alex Suciu - alexsuciu06@gmail.com
+*/
+
+var canvas = document.getElementById('canvas'),
+	running = false,
+	placedRight = [];
+	
+function drawTemplateBike() {
+	var img = new Image();
+
+	img.onload = function() {
+		$(canvas).drawImage({
+			source: img,
+			x: 246, y: 331,
+			width: img.width / 4,
+			height: img.height / 4,
+			opacity: 0.1,
+			fromCenter: false,
+			layer: true,
+			click: function(layer) { console.log(layer); }
+		});
+	}
+	img.src = "./static/bike.png";
+}
+
+function drawParts() {
+	for (var ix in imgs) {
+		let image = new Image();
+
+		image.name = imgs[ix].name;
+		image.id = ix;
+		image.onload = function() { renderPart(image) }
+		image.src = imgs[ix].src;
+	}
+}
+
+function renderPart(image) {
+	let w = image.width / 4,
+		h = image.height / 4;
+		
+	$(canvas).drawImage({
+		source: image,
+		x: imgs[image.id].x, 
+		y: imgs[image.id].y,
+		width: w, height: h,
+		fromCenter: false,
+		draggable: true,
+		layer: true,
+		bringToFront: true,
+		click: function(layer) { 
+			console.log(layer);
+			handlePartClick(layer);
+			},
+		mousedown: function(layer) {
+			console.log("mousedown" + layer);
+			},
+		mouseup: function(layer) {
+			console.log("mouseup" + layer);
+			checkSolution(layer);
+			return false;
+			}
+	});
+}	
+
+function rect(x, y, width, height) {
+	$(canvas).drawRect({
+			fillStyle: "transparent",
+			strokeStyle: "#FFA500", // orange
+			strokeWidth: "2",
+			visible: true,
+			x: x, y: y,
+			width: width + 4,
+			height: height + 4,
+			fromCenter: false
+		});
+}
+
+function handlePartClick(layer) {
+	let name = layer.source.name;
+	rect(layer.x, layer.y, layer.width, layer.height);
+	
+	if (running) {
+		$.stopSound();
+		running = false;
+		return false;
+	}
+	_playSound(name);
+	return false;
+}
+
+function checkSolution(layer) {
+	// index: -1 if element is not in list, else the index where we find it
+	let index = placedRight.indexOf(layer.source.name);
+	
+	if ( isPlacedRight(layer) ) {
+		stickyLayer(layer);
+		if ( index == -1 ) {
+			placedRight.push(layer.source.name);
+		}
+		if ( placedRight.length === 7 ) {
+			showCongrats();
+		}
+	} else {
+ 
+		if ( index != -1 ) {
+			placedRight.splice(index, 1);
+		}
+	}
+}
+
+function isPlacedRight(layer) {
+	let sol = solutions[layer.source.name],
+		dx = Math.abs(layer.x - sol.x),
+		dy = Math.abs(layer.y - sol.y);
+		
+	return (dx < 10 && dy < 10);
+	
+}
+
+function stickyLayer(layer) {
+	let name = layer.source.name,
+		sol = solutions[name];
+	
+	layer.x = sol.x;
+	layer.y = sol.y;
+}
+
+function randomCongratsGIF() {
+	let r = Math.floor(1 + 7 * Math.random()); // random int in [1, 7]
+	return "\"./static/congrats/solved" + r + ".gif\"";
+}
+
+function showCongrats() {
+	var congratsImg = randomCongratsGIF();
+	$('#content').html(
+		"<div id=\"congrats_heder\">FELICITARI!!!</div>"
+	  +	"<img id=\"congrats_gif\" src=" + congratsImg + " />"
+		);
+}
+
+function _playSound(name) {
+	
+	if (sounds[name]) {
+		running = true;
+		$.playSound(sounds[name])
+			.on("ended", function() {
+				running = false;
+			});
+	}
+}
